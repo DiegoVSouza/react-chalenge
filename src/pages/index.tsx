51bb2fed1cd796eb1fd5bedb039308e27d004209
DiegoVSouza <dvsdiegovieira@gmail.com>
2022-09-21 10:53:25 -0300

    feat: Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
-import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from '../services/prismicio';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +26,65 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  console.log();
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
+      <main className={styles.postContainer}>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+        <div className={styles.postsContent}>
+      <img src="/logo.svg" alt="logo" />
 
-//   // TODO
-// };
+          {postsPagination.results.map(post => (
+            <Link href={`/post/${post.uid}`}>
+              <a key={post.uid}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <time>{post.first_publication_date}</time>
+                <label>{post.data.author}</label>
+
+              </a>
+            </Link>
+          ))}
+        </div>
+        <button className={postsPagination.next_page ? '' : styles.hidden}>Carregar mais posts</button>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', {
+    pageSize: 2,
+  });
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.last_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
